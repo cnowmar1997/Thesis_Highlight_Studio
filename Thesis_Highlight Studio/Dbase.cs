4ad1ddad3eb37a7aee983ace2d25502e3d558c20
Nowmar Calidro <cnowmar1997@users.noreply.github.com>
2017-07-02 04:33:10 +0800

    Database File

---
+++ Thesis_Highlight Studio/Dbase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace Thesis_Highlight_Studio
+{
+    class Dbase
+    {
+        private static MySqlConnection con;
+        public static MySqlConnection Con
+        {
+            get
+            {
+                if (con == null)
+                {
+                    con = new MySqlConnection(ConString());
+                    con.Open();
+                }
+                return con;
+            }
+        }
+
+        protected static string defaultConnectionString = "server=localhost; User id=root; password=; database=redcrossdb";
+        protected static string filepath = AppDomain.CurrentDomain.BaseDirectory.ToString();
+        protected static string filename = "dbserver.cfg";
+        private static string ConString()
+        {
+            string file = filepath+filename;
+            string ret = null;
+
+            //create dbserver.cfg if it does not exist
+            if (!File.Exists(file))
+            {
+                CMsgBox.Show(filename + " not found! Creating a new configuration file...");
+                using (StreamWriter sw = new StreamWriter(file, false))
+                {
+                    sw.WriteLine();
+                    sw.Close();
+                }
+            }
+
+            using (Stream filestream = File.Open(file, FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(filestream))
+                {
+                    ret = string.IsNullOrEmpty(sr.ReadLine()) ? defaultConnectionString : sr.ReadLine();
+                    sr.Close();
+                }
+                filestream.Close();
+            }
+            return ret;
+        }
+    }
+}
